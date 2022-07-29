eb6cd47c67afe634171b015f674d683fefdd4205
hollowblind095 <hollowblind095@gmail.com>
2022-07-29 18:40:26 -0300

    add footer test

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <footer className="container">Prueba de componente footer</footer>
+  )
+}
+
+export default Footer
