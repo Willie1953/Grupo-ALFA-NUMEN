869d5f6cf0dd2c910fb96bdc9911ebf58f46c34f
Willie1953 <gjepilar@yahoo.com>
2022-08-26 18:36:45 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Footer from "./components/Footer";
+import Footer from "./Footer/Footer";
+import Carousel from "./Carousel/Carousel";
+
+
 
 function App() {
   return (
     <>
     <header>Header</header>
     <main>Principal</main>
-    <div className="App-footer w-full h-fit text-xs text-center bg-white-200">
-        <Footer/>
-    </div>
+    <Carousel/>
+    <Footer/>
     </>
   );
 }
