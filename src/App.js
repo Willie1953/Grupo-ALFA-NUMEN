14f9d523695a70b1b2c0c335846f9ec207fbb138
leonardoh82 <leo.a.heredia@gmail.com>
2022-09-02 19:28:36 -0300

    new merge guillermo-new dev-leo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Advertencia from "./components/Advertencia";
 import Slide from "./components/Carousel";
 import Footer from "./components/Footer";
 import { Head } from "./components/Head";
+import Banner from "./components/Banner";
+import Quienessomos from "./components/Quienessomos";
 
 function App() {
   return (
